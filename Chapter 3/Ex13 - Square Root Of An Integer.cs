dd4a9ccfe707bdf88030fe2443dbdd68c6fee204
A-N-R-I <100511277+A-N-R-I@users.noreply.github.com>
2022-12-15 07:28:36 +0100

    Create Ex13 - Square Root Of An Integer.cs

--- Chapter 3/Ex13 - Square Root Of An Integer.cs
+++ Chapter 3/Ex13 - Square Root Of An Integer.cs
@@ -3,8 +3,9 @@ int n = int.Parse(Console.ReadLine());
 
 // 0 and 1 aren't prime. 0 can be divided by any number and 1 is ...well, 1
 bool isPrime = (n == 0 || n == 1)? false : true;
+int N = (int)Math.Sqrt(n);
 
-for (int i = 2; i <= Math.Sqrt(n); ++i)
+for (int i = 2; i <= N; ++i)
 {
     if (n%i == 0)
     {
