8637ee0816ae3ac4ef075c2877d0b609115d01e7
A-N-R-I <100511277+A-N-R-I@users.noreply.github.com>
2023-01-02 21:54:32 +0100

    Create Ex08 - Maximum Of Five Integers.cs

---
+++ Chapter 4/Ex08 - Maximum Of Five Integers.cs
@@ -0,0 +1,12 @@
+Console.Write("Enter five(5) integers: ");
+string[] input = Console.ReadLine().Split();
+
+// Assume an initial maximum value
+int max = int.Parse(input[0]);
+
+for (int i = 1; i < input.Length; ++i)
+{
+    max = System.Math.Max(max, int.Parse(input[i]));
+}
+
+Console.WriteLine($"Maximum value: {max}");
